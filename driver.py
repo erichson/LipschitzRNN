c861abe88dc75face6148d27ead1d0f991f9465c
erichson <erichson@uw.edu>
2020-06-02 13:15:45 -0700

    streamlining

--- driver.py
+++ driver.py
@@ -22,7 +22,7 @@ from models import *
 
 parser = argparse.ArgumentParser(description='MNIST Example')
 #
-parser.add_argument('--name', type=str, default='mnist', metavar='N', help='dataset')
+parser.add_argument('--name', type=str, default='cifar10', metavar='N', help='dataset')
 #
 parser.add_argument('--batch-size', type=int, default=128, metavar='N', help='input batch size for training (default: 128)')
 #
@@ -40,17 +40,17 @@ parser.add_argument('--wd', default=0.0, type=float, metavar='W', help='weight d
 #
 parser.add_argument('--gamma', default=0.001, type=float, metavar='W', help='diffiusion rate')
 #
-parser.add_argument('--beta', default=0.65, type=float, metavar='W', help='skew level')
+parser.add_argument('--beta', default=0.7, type=float, metavar='W', help='skew level')
 #
 parser.add_argument('--model', type=str, default='LipschitzRNN', metavar='N', help='model name')
 #
-parser.add_argument('--n_units', type=int, default=128, metavar='S', help='number of hidden units')
+parser.add_argument('--n_units', type=int, default=16, metavar='S', help='number of hidden units')
 #
 parser.add_argument('--eps', default=0.1, type=float, metavar='W', help='time step for euler scheme')
 #
 parser.add_argument('--gated', default=False, type=bool, metavar='W', help='gated')
 #
-parser.add_argument('--T', default=49, type=int, metavar='W', help='time steps')
+parser.add_argument('--T', default=1024, type=int, metavar='W', help='time steps')
 #
 parser.add_argument('--init_std', type=float, default=12, metavar='S', help='control of std for initilization')
 #
@@ -82,15 +82,31 @@ device = get_device()
 #==============================================================================
 # get dataset
 #==============================================================================
-train_loader, test_loader = getData(name=args.name, train_bs=args.batch_size, test_bs=args.test_batch_size)
-print('data is loaded')
 
-for data, target in train_loader:
-    data, target = data.to(device), target.to(device)
 
-model = rnn_models(input_dim=int(784/args.T), output_classes=10, n_units=args.n_units, 
+
+
+if args.name == 'mnist' or args.name == 'pmnist':
+    train_loader, test_loader = getData(name='mnist', train_bs=args.batch_size, test_bs=args.test_batch_size)  
+    model = rnn_models(input_dim=int(784/args.T), output_classes=10, n_units=args.n_units, 
                  eps=args.eps, beta=args.beta, gamma=args.gamma, gated=args.gated,
                  model=args.model, init_std=args.init_std, alpha=args.alpha).to(device)
+            
+elif args.name == 'cifar10':    
+    train_loader, test_loader = getData(name='cifar10', train_bs=args.batch_size, test_bs=args.test_batch_size)          
+    model = rnn_models(input_dim=int(1024/args.T*3), output_classes=10, n_units=args.n_units, 
+                 eps=args.eps, beta=args.beta, gamma=args.gamma, gated=args.gated,
+                 model=args.model, init_std=args.init_std).to(device) 
+
+elif args.name == 'cifar10_noise':  
+    train_loader, test_loader = getData(name='cifar10', train_bs=args.batch_size, test_bs=args.test_batch_size)              
+    model = rnn_models(input_dim=int(1024/args.T*3), output_classes=10, n_units=args.n_units, 
+                 eps=args.eps, beta=args.beta, gamma=args.gamma, gated=args.gated,
+                 model=args.model, init_std=args.init_std).to(device)     
+    
+    torch.manual_seed(args.seed)
+    torch.cuda.manual_seed(args.seed)        
+    noise = torch.randn(1,968,32,3).float()
 
 #==============================================================================
 # Model summary
@@ -125,19 +141,30 @@ for epoch in range(args.epochs):
     
     for step, (x, y) in enumerate(train_loader):
         count += 1
-        inputs = Variable(x.view(-1, args.T, int(784/args.T))).to(device) # reshape x to (batch, time_step, input_size)
-        targets = Variable(y).to(device)
+        
+        # Reshape data for recurrent unit
+        if args.name == 'mnist' or args.name == 'pmnist':
+            inputs = Variable(x.view(-1, args.T, int(784/args.T))).to(device) # reshape x to (batch, time_step, input_size)
+            targets = Variable(y).to(device)
+            
+        elif args.name == 'cifar10':            
+            inputs = Variable(x.view(-1, args.T, int(1024/args.T*3))).to(device) # reshape x to (batch, time_step, input_size)
+            targets = Variable(y).to(device)   
 
-        output = model(inputs)   # pred = F(inputs)
-        loss = loss_func(output, targets)   # compute loss 
+        elif args.name == 'cifar10_noise':
+            x = x.view(-1, 32, int(96))
+            x = torch.cat((x, noise.repeat(x.shape[0],1,1,1).view(-1, 968, int(96))), 1) # reshape x to (batch, time_step, input_size)
+            inputs = Variable(x).to(device)             
+            targets = Variable(y).to(device)   
+
+                 
+        # send data to recurrent unit    
+        output = model(inputs)   
+        loss = loss_func(output, targets)
         
-        if args.model == 'lizRNN_test':
-            where = model.B.data > 0.0
-            loss += torch.sum(model.B.data[where])
         
-        
-        optimizer.zero_grad()               # clear gradients for this training step
-        loss.backward()                     # compute gradients with respect to loss
+        optimizer.zero_grad()
+        loss.backward()                 
         torch.nn.utils.clip_grad_norm_(model.parameters(), args.gclip) # gradient clip
         optimizer.step() # update weights
         lossaccum += loss.item()
@@ -201,7 +228,13 @@ for epoch in range(args.epochs):
     optimizer=exp_lr_scheduler(epoch, optimizer, decay_eff=args.lr_decay, decayEpoch=args.lr_decay_epoch)
 
 #torch.save(model.state_dict(), args.name + '_results/' + args.model + '_' + str(args.T) + str(args.n_units) +'.pkl')  
-torch.save(model, args.name + '_results/' + args.model + '_T' + str(args.T) + '_units' + str(args.n_units) +'.pkl')  
+torch.save(model, args.name + '_results/' + args.model + '_' + args.name + '_T' 
+           + str(args.T) + '_units' + str(args.n_units) + '_beta' + str(args.beta) 
+           + '_gamma' + str(args.gamma) + '_eps' + str(args.eps) 
+           + '_init' + str(args.init_std) + '_init' + str(args.init_std) 
+           + '_seed' + str(args.seed) + '.pkl')  
+
+
 
 data = {'loss': lossaccum, 'eigA': max_eig_A, 'eigW': max_eig_W, 'testacc': test_acc}
 f = open(args.name + '_results/' + args.model + '_T' + str(args.T) + '_units' + str(args.n_units) + '_loss.pkl',"wb")
