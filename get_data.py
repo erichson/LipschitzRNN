5de6b7dd5b8c60702be87bdbc9f9829b74cf522c
Alejandro Francisco Queiruga <afq@google.com>
2020-11-20 00:16:08 -0800

    yapify

--- get_data.py
+++ get_data.py
@@ -14,112 +14,147 @@ from torch.utils.data import Subset
 from torch._utils import _accumulate
 
 
-def getData(name='cifar10', train_bs=128, test_bs=1000):    
-    
-   
-    
+def getData(name='cifar10', train_bs=128, test_bs=1000):
+
     if name == 'mnist':
 
-        train_loader = datasets.MNIST('./data', train=True, download=True,
-                           transform=transforms.Compose([transforms.ToTensor(),]))
+        train_loader = datasets.MNIST('./data',
+                                      train=True,
+                                      download=True,
+                                      transform=transforms.Compose([
+                                          transforms.ToTensor(),
+                                      ]))
 
-        val_loader = datasets.MNIST('./data', train=True, download=True,
-                           transform=transforms.Compose([transforms.ToTensor(),]))
+        val_loader = datasets.MNIST('./data',
+                                    train=True,
+                                    download=True,
+                                    transform=transforms.Compose([
+                                        transforms.ToTensor(),
+                                    ]))
 
         offset = 3000
         rng = np.random.RandomState(1234)
         R = rng.permutation(len(train_loader))
         lengths = (len(train_loader) - offset, offset)
-        train_loader, val_loader = [Subset(train_loader, R[offset - length:offset]) for offset, length in zip(_accumulate(lengths), lengths)]
+        train_loader, val_loader = [
+            Subset(train_loader, R[offset - length:offset])
+            for offset, length in zip(_accumulate(lengths), lengths)
+        ]
 
+        train_loader = torch.utils.data.DataLoader(train_loader,
+                                                   batch_size=train_bs,
+                                                   shuffle=True)
+        val_loader = torch.utils.data.DataLoader(val_loader,
+                                                 batch_size=test_bs,
+                                                 shuffle=False)
 
-        train_loader = torch.utils.data.DataLoader(train_loader, batch_size=train_bs, shuffle=True)
-        val_loader = torch.utils.data.DataLoader(val_loader, batch_size=test_bs, shuffle=False)
-
-
-        test_loader = torch.utils.data.DataLoader(
-            datasets.MNIST('./data', train=False, download=False,
-            transform=transforms.Compose([transforms.ToTensor(),])),
-            batch_size=test_bs, shuffle=False)
-
+        test_loader = torch.utils.data.DataLoader(datasets.MNIST(
+            './data',
+            train=False,
+            download=False,
+            transform=transforms.Compose([
+                transforms.ToTensor(),
+            ])),
+                                                  batch_size=test_bs,
+                                                  shuffle=False)
 
     if name == 'pmnist':
 
-        trainset = datasets.MNIST(root='./data', train=True, download=True,
-                            transform=transforms.Compose([transforms.ToTensor(),]))
-        
-        testset = datasets.MNIST(root='./data', train=False, download=False,
-                            transform=transforms.Compose([ transforms.ToTensor(),]))
-        
+        trainset = datasets.MNIST(root='./data',
+                                  train=True,
+                                  download=True,
+                                  transform=transforms.Compose([
+                                      transforms.ToTensor(),
+                                  ]))
+
+        testset = datasets.MNIST(root='./data',
+                                 train=False,
+                                 download=False,
+                                 transform=transforms.Compose([
+                                     transforms.ToTensor(),
+                                 ]))
 
         x_train = trainset.train_data
         y_train = trainset.targets
-        
-        x_test = testset.test_data        
+
+        x_test = testset.test_data
         y_test = testset.targets
 
-
-        torch.manual_seed(12008)        
+        torch.manual_seed(12008)
         perm = torch.randperm(784)
 
-        x_train_permuted = x_train.reshape(x_train.shape[0],-1)
+        x_train_permuted = x_train.reshape(x_train.shape[0], -1)
         x_train_permuted = x_train_permuted[:, perm]
         x_train_permuted = x_train_permuted.reshape(x_train.shape[0], 28, 28)
-        
-        x_test_permuted = x_test.reshape(x_test.shape[0],-1)
+
+        x_test_permuted = x_test.reshape(x_test.shape[0], -1)
         x_test_permuted = x_test_permuted[:, perm]
-        x_test_permuted = x_test_permuted.reshape(x_test.shape[0], 28, 28)        
+        x_test_permuted = x_test_permuted.reshape(x_test.shape[0], 28, 28)
 
         x_train_permuted = add_channels(x_train_permuted)
         x_test_permuted = add_channels(x_test_permuted)
-        
-        train_loader = torch.utils.data.TensorDataset(x_train_permuted.float(), y_train)
-        
+
+        train_loader = torch.utils.data.TensorDataset(x_train_permuted.float(),
+                                                      y_train)
 
         offset = 3000
         rng = np.random.RandomState(1234)
         R = rng.permutation(len(train_loader))
         lengths = (len(train_loader) - offset, offset)
-        train_loader, val_loader = [Subset(train_loader, R[offset - length:offset]) for offset, length in zip(_accumulate(lengths), lengths)]
+        train_loader, val_loader = [
+            Subset(train_loader, R[offset - length:offset])
+            for offset, length in zip(_accumulate(lengths), lengths)
+        ]
 
+        train_loader = torch.utils.data.DataLoader(train_loader,
+                                                   batch_size=train_bs,
+                                                   shuffle=True)
+        val_loader = torch.utils.data.DataLoader(val_loader,
+                                                 batch_size=test_bs,
+                                                 shuffle=False)
 
-        train_loader = torch.utils.data.DataLoader(train_loader, batch_size=train_bs, shuffle=True)
-        val_loader = torch.utils.data.DataLoader(val_loader, batch_size=test_bs, shuffle=False)
-        
-        test_loader = torch.utils.data.DataLoader(torch.utils.data.TensorDataset(x_test_permuted.float(), y_test),
-                                                batch_size=test_bs, shuffle=False)
-      
-    
-    
-    
+        test_loader = torch.utils.data.DataLoader(
+            torch.utils.data.TensorDataset(x_test_permuted.float(), y_test),
+            batch_size=test_bs,
+            shuffle=False)
+
     if name == 'cifar10':
         transform_train = transforms.Compose([
-        transforms.ToTensor(),
-    ])
+            transforms.ToTensor(),
+        ])
 
         transform_test = transforms.Compose([
-        transforms.ToTensor(),
-    ])
+            transforms.ToTensor(),
+        ])
 
-        train_loader = datasets.CIFAR10(root='./data', train=True, download=True, transform=transform_train)
-
+        train_loader = datasets.CIFAR10(root='./data',
+                                        train=True,
+                                        download=True,
+                                        transform=transform_train)
 
         offset = 3000
         rng = np.random.RandomState(1234)
         R = rng.permutation(len(train_loader))
         lengths = (len(train_loader) - offset, offset)
-        train_loader, val_loader = [Subset(train_loader, R[offset - length:offset]) for offset, length in zip(_accumulate(lengths), lengths)]
+        train_loader, val_loader = [
+            Subset(train_loader, R[offset - length:offset])
+            for offset, length in zip(_accumulate(lengths), lengths)
+        ]
 
+        train_loader = torch.utils.data.DataLoader(train_loader,
+                                                   batch_size=train_bs,
+                                                   shuffle=True)
+        val_loader = torch.utils.data.DataLoader(val_loader,
+                                                 batch_size=test_bs,
+                                                 shuffle=False)
 
-        train_loader = torch.utils.data.DataLoader(train_loader, batch_size=train_bs, shuffle=True)
-        val_loader = torch.utils.data.DataLoader(val_loader, batch_size=test_bs, shuffle=False)
-
-
-        testset = datasets.CIFAR10(root='./data', train=False, download=False, transform=transform_test)
-        test_loader = torch.utils.data.DataLoader(testset, batch_size=test_bs, shuffle=False)
-    
-    
-
+        testset = datasets.CIFAR10(root='./data',
+                                   train=False,
+                                   download=False,
+                                   transform=transform_test)
+        test_loader = torch.utils.data.DataLoader(testset,
+                                                  batch_size=test_bs,
+                                                  shuffle=False)
 
     if name == 'double_pendulum':
         # open a file, where you stored the pickled data
@@ -131,37 +166,37 @@ def getData(name='cifar10', train_bs=128, test_bs=1000):
         train_target = []
         testset = []
         test_target = []
-        
-        for i in range(1,400):
-            trainset.append(data[i:i+1000])
-            train_target.append(data[i+1000+1])
-        
-        for i in range(1501,3000):
-            testset.append(data[i:i+1000])
-            test_target.append(data[i+1000+1])
-                
+
+        for i in range(1, 400):
+            trainset.append(data[i:i + 1000])
+            train_target.append(data[i + 1000 + 1])
+
+        for i in range(1501, 3000):
+            testset.append(data[i:i + 1000])
+            test_target.append(data[i + 1000 + 1])
+
         trainset = np.asarray(trainset)
         testset = np.asarray(testset)
         train_target = np.asarray(train_target)
-        test_target = np.asarray(test_target)        
-        
+        test_target = np.asarray(test_target)
+
         trainset = torch.tensor(trainset)
         testset = torch.tensor(testset)
         train_target = torch.tensor(train_target)
-        test_target = torch.tensor(test_target)        
+        test_target = torch.tensor(test_target)
 
         #trainset = add_channels(trainset)
-        #testset = add_channels(testset)       
-            
-        train_loader = torch.utils.data.DataLoader(torch.utils.data.TensorDataset(trainset.float(), train_target.float()), batch_size=train_bs, shuffle=True)
-        test_loader = torch.utils.data.DataLoader(torch.utils.data.TensorDataset(testset.float(), test_target.float()), batch_size=test_bs, shuffle=False)
-    
+        #testset = add_channels(testset)
+
+        train_loader = torch.utils.data.DataLoader(
+            torch.utils.data.TensorDataset(trainset.float(),
+                                           train_target.float()),
+            batch_size=train_bs,
+            shuffle=True)
+        test_loader = torch.utils.data.DataLoader(
+            torch.utils.data.TensorDataset(testset.float(),
+                                           test_target.float()),
+            batch_size=test_bs,
+            shuffle=False)
 
     return train_loader, test_loader, val_loader
-
-
-
-
-
-
-
--- tools.py
+++ tools.py
@@ -12,7 +12,7 @@ from torch.utils.data import Subset
 
 def get_device():
     """Get a gpu if available."""
-    if torch.cuda.device_count()>0:
+    if torch.cuda.device_count() > 0:
         device = torch.device('cuda')
         print("Connected to a GPU")
     else:
@@ -20,27 +20,30 @@ def get_device():
         device = torch.device('cpu')
     return device
 
+
 def which_device(model):
     return next(model.parameters()).device
 
 
 def add_channels(X):
     if len(X.shape) == 2:
-        return X.reshape(X.shape[0], 1, X.shape[1],1)
+        return X.reshape(X.shape[0], 1, X.shape[1], 1)
 
     elif len(X.shape) == 3:
         return X.reshape(X.shape[0], 1, X.shape[1], X.shape[2])
 
     else:
         return "dimenional error"
-    
-    
-    
 
-def exp_lr_scheduler(epoch, optimizer, strategy='normal', decay_eff=0.1, decayEpoch=[]):
+
+def exp_lr_scheduler(epoch,
+                     optimizer,
+                     strategy='normal',
+                     decay_eff=0.1,
+                     decayEpoch=[]):
     """Decay learning rate by a factor of lr_decay every lr_decay_epoch epochs"""
 
-    if strategy=='normal':
+    if strategy == 'normal':
         if epoch in decayEpoch:
             for param_group in optimizer.param_groups:
                 param_group['lr'] *= decay_eff
@@ -51,11 +54,9 @@ def exp_lr_scheduler(epoch, optimizer, strategy='normal', decay_eff=0.1, decayEp
 
     return optimizer
 
-    
-    
-def gaussian_init_(n_units, std=1):    
-    sampler = torch.distributions.Normal(torch.Tensor([0]), torch.Tensor([std/n_units]))
-    A_init = sampler.sample((n_units, n_units))[..., 0]  
-    return A_init
 
-        
+def gaussian_init_(n_units, std=1):
+    sampler = torch.distributions.Normal(torch.Tensor([0]),
+                                         torch.Tensor([std / n_units]))
+    A_init = sampler.sample((n_units, n_units))[..., 0]
+    return A_init
